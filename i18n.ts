464930d48b9184a1ec5b02976d6d69e3bd4c06ba
--global <maxim_united@yahoo.com>
2025-08-09 21:24:31 +0300

    i18n(next-intl): export getRequestConfig to satisfy config discovery

--- i18n.ts
+++ i18n.ts
@@ -1,9 +1,14 @@
-// Minimal next-intl config for App Router
-// See: https://next-intl.dev/docs/getting-started/app-router
+// next-intl config for App Router
+// https://next-intl.dev/docs/getting-started/app-router
+import {getRequestConfig} from 'next-intl/server';
 
 export const locales = ['he', 'en'] as const;
 export type Locale = (typeof locales)[number];
-
 export const defaultLocale: Locale = 'he';
+
+export default getRequestConfig(() => ({
+  locales: Array.from(locales) as unknown as string[],
+  defaultLocale
+}));
 
 
